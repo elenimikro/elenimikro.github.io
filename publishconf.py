d4bdf03fccecf28b02beba1a6c86ed13ba6d8532
Eleni Mikroyannidi <mikrohelen@gmail.com>
2016-09-18 19:29:48 +0100

    Updated site

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.mikrohelen.com'
+SITEURL = 'https://elenimikro.github.io/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
